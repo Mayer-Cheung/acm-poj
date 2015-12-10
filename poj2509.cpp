ab706ed7de35d58ac79bccec9242bf957857a99b
mayer cheung <mayercheung@gmail.com>
2015-12-10 16:29:31 -0500

    15/12/10

---
+++ poj2509.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+int main()
+{
+	int n, k;
+	while (scanf("%d%d", &n, &k) != EOF)
+	{
+		int sum = n;
+		while (n / k)
+		{
+			sum += n / k;
+			n = n % k + n / k;
+		}
+		printf("%d\n", sum);
+	}
+}

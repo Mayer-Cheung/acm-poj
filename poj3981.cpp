70e27bad3b12657e2242e69bbd15408a74118d3d
mayer cheung <mayercheung@gmail.com>
2016-01-26 08:33:16 -0500

    16/1/26

---
+++ poj3981.cpp
@@ -0,0 +1,18 @@
+#include <cstring>
+#include <string>
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+int main()
+{
+	string str;
+	int pos;
+	while (getline(cin, str))
+	{
+		while ((pos = str.find("you")) != -1)
+			str.replace(pos, 3, "we");
+		cout << str << endl;
+	}
+}
